4cf5d7b1a17cff454fbf62a99a2daa184a2495a8
hideki kuno <hidekuno@gmail.com>
2023-07-08 17:43:50 +0900

    初期表示のテストケースを追加した

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,9 @@
 import React from "react"
 import { Provider } from "react-redux"
 
-import { render, screen, waitFor, cleanup } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import '@testing-library/jest-dom'
+
 import App from "../App"
 import { configureStore } from "../store"
 
@@ -18,11 +20,10 @@ const response = class {
 }
 global.fetch = jest.fn(() => new response("public/cd.json"))
 
-afterEach(cleanup)
 describe('unit test', () => {
-  let component
 
   test("snapshot test ", async () => {
+    let component
     await waitFor(() => {
       component = render(<Provider store={store}>
                          <React.StrictMode>
@@ -31,5 +32,28 @@ describe('unit test', () => {
                          </Provider>)
     })
     expect(component.container).toMatchSnapshot()
+  })
+
+  test("initial test ", async () => {
+    await waitFor(() => {
+      render(<Provider store={store}>
+             <React.StrictMode>
+             <App />
+             </React.StrictMode>
+             </Provider>)
+    })
+    expect(screen.getAllByRole("button", { name: 'Add to Cart' })).toHaveLength(10)
+
+    const data = fetch("public/cd.json").json()
+    data.forEach(element => {
+      expect(screen.getByText(element.title)).toBeInTheDocument()
+
+      expect(screen.getAllByText(element.artist)).toHaveLength(data.filter((n) => (n.artist == element.artist)).length)
+      expect(screen.getAllByText('$' + element.price)).toHaveLength(data.filter((n) => (n.price == element.price)).length)
+
+      expect(screen.getByText(element.description)).toBeInTheDocument()
+      expect(screen.getByAltText(element.title).src).toContain(element.imageUrl)
+    })
+    expect(screen.getByText('There are no items in your cart.')).toBeInTheDocument()
   })
 })
