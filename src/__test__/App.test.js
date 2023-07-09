d3643a84cfbdcdc376651d82f9ba47d3fb7aa3ed
hideki kuno <hidekuno@gmail.com>
2023-07-09 11:18:21 +0900

    テストケース(商品選択・削除・決済)を追加

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Provider } from "react-redux"
 
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen, waitFor, fireEvent } from "@testing-library/react";
 import '@testing-library/jest-dom'
 
 import App from "../App"
@@ -48,12 +48,92 @@ describe('unit test', () => {
     data.forEach(element => {
       expect(screen.getByText(element.title)).toBeInTheDocument()
 
-      expect(screen.getAllByText(element.artist)).toHaveLength(data.filter((n) => (n.artist == element.artist)).length)
-      expect(screen.getAllByText('$' + element.price)).toHaveLength(data.filter((n) => (n.price == element.price)).length)
+      expect(screen.getAllByText(element.artist)).toHaveLength(
+        data.filter((n) => (n.artist == element.artist)).length)
+      expect(screen.getAllByText('$' + element.price)).toHaveLength(
+        data.filter((n) => (n.price == element.price)).length)
 
       expect(screen.getByText(element.description)).toBeInTheDocument()
       expect(screen.getByAltText(element.title).src).toContain(element.imageUrl)
     })
     expect(screen.getByText('There are no items in your cart.')).toBeInTheDocument()
+  })
+
+  test("add to cart click 1 test ", async () => {
+    await waitFor(() => {
+      render(<Provider store={store}>
+             <React.StrictMode>
+             <App />
+             </React.StrictMode>
+             </Provider>)
+    })
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByRole("button", { name: 'Add to Cart' })[0])
+    })
+    expect(screen.getByText('In your cart')).toBeInTheDocument()
+    expect(screen.getByText('Total Amount: $25')).toBeInTheDocument()
+
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByRole("button", { name: 'Add to Cart' })[1])
+    })
+    expect(screen.getByText('Total Amount: $48')).toBeInTheDocument()
+
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByRole("button", { name: 'Add to Cart' })[2])
+    })
+    expect(screen.getByText('Total Amount: $69')).toBeInTheDocument()
+  })
+
+  test("delete click test ", async () => {
+    await waitFor(() => {
+      render(<Provider store={store}>
+             <React.StrictMode>
+             <App />
+             </React.StrictMode>
+             </Provider>)
+    })
+    await waitFor(() => {
+     fireEvent.click(screen.getAllByRole("button", { name: 'Delete' })[2])
+    })
+    await waitFor(() => {
+     fireEvent.click(screen.getAllByRole("button", { name: 'Delete' })[1])
+    })
+    expect(screen.getByText('Total Amount: $21')).toBeInTheDocument()
+    await waitFor(() => {
+     fireEvent.click(screen.getAllByRole("button", { name: 'Delete' })[0])
+    })
+    expect(screen.getByText('There are no items in your cart.')).toBeInTheDocument()
+  })
+  test("add to cart click 2unit test ", async () => {
+    await waitFor(() => {
+      render(<Provider store={store}>
+             <React.StrictMode>
+             <App />
+             </React.StrictMode>
+             </Provider>)
+    })
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByRole("button", { name: 'Add to Cart' })[0])
+    })
+    await waitFor(() => {
+      fireEvent.click(screen.getAllByRole("button", { name: 'Add to Cart' })[0])
+    })
+    expect(screen.getByText('Total Amount: $50')).toBeInTheDocument()
+    expect(screen.getByText('2')).toBeInTheDocument()
+  })
+  test("purchase click test ", async () => {
+    await waitFor(() => {
+      render(<Provider store={store}>
+             <React.StrictMode>
+             <App />
+             </React.StrictMode>
+             </Provider>)
+    })
+    await waitFor(() => {
+      fireEvent.click(screen.getByRole("button", { name: 'Purchase' }))
+    })
+    expect(screen.getByText('Your Point: $100')).toBeInTheDocument()
+    expect(screen.getAllByText('Total Amount: $50')).toHaveLength(2)
+    expect(screen.getByText('Would you like to buy?')).toBeInTheDocument()
   })
 })
